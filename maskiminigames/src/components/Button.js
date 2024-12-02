f62b58e463ec9d00368b39dcc617e9366efb1270
Ayleen <ayleenpt@uw.edu>
2024-12-02 13:30:06 -0800

    add temp links, adjust style to be set in .css not .js

--- maskiminigames/src/components/Button.js
+++ maskiminigames/src/components/Button.js
@@ -1,29 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ link, altText, customStyle }) => {
-    const defaultStyle = {
-      display: 'inline-block',
-      width: '100px',
-      height: '50px',
-      backgroundSize: 'cover',
-      backgroundRepeat: 'no-repeat',
-      backgroundPosition: 'center',
-      cursor: 'pointer',
-      ...customStyle, // Merge with custom styles passed as props
-    };
-    return (
-        <a href={link} target="_blank" rel="noopener noreferrer" style={defaultStyle} aria-label={altText}>
-            {/* No <img> tag; the image is handled as a background */}
-        </a>
+const Button = ({ link, altText, className }) => {
+  return (
+      <a
+        className={className}
+        href={link}
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label={altText}
+      >
+        {}
+      </a>
     );
 };
 
 // Define prop types
 Button.propTypes = {
-    link: PropTypes.string.isRequired, // Link is still required
-    altText: PropTypes.string,         // Accessibility text
-    customStyle: PropTypes.object,     // Optional style overrides
+    link: PropTypes.string.isRequired,            // Link to redirect
+    altText: PropTypes.string,                    // Accessibility text
+    className: PropTypes.string.isRequired                   // CSS class name
 };
 
 export default Button;
--- maskiminigames/src/components/Homepage.js
+++ maskiminigames/src/components/Homepage.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import logo from '../logo.svg';
 import Button from './Button.js';
-import snakeButton from '../assets/snake_button.png';
-import endlessRacingButton from '../assets/endless_racing_button.png';
 import '../styles/Homepage.css';
 
 const Homepage = () => {
@@ -11,31 +9,15 @@ const Homepage = () => {
       <img src={logo} className="App-logo" alt="logo" />
       <div className="buttons">
         <Button 
-          link="link_to_snake_game" // TODO: Insert link to snake game page
+          link="https://dev.azure.com/software-engineering-studio/studio-course/_workitems/edit/12014/" // TODO: Insert link to snake game page
           altText="Play Snake"
-          customStyle={{
-            display: 'inline-block',
-            width: '300px',
-            height: '205px',
-            backgroundImage: `url(${snakeButton})`,
-            backgroundSize: 'cover',
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: 'center',
-          }}
+          className="button snake-button"
         />
 
         <Button 
-          link="link_to_endless_racing_game" // TODO: Insert link to endless racing game page
+          link="https://dev.azure.com/software-engineering-studio/studio-course/_workitems/edit/12020/" // TODO: Insert link to endless racing game page
           altText="Play Endless Racing"
-          customStyle={{
-            display: 'inline-block',
-            width: '300px',
-            height: '205px',
-            backgroundImage: `url(${endlessRacingButton})`,
-            backgroundSize: 'cover',
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: 'center',
-          }}
+          className="button endless-racing-button"
         />
       </div>
     </header>
