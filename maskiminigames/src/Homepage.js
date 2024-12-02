6efcb8db0b92398fc1abdc367999695dd4ae06a6
Ayleen <ayleenpt@uw.edu>
2024-12-01 17:50:04 -0800

    create homepage component with logo and background

---
+++ maskiminigames/src/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './Homepage.css';
+
+const Homepage = () => {
+  return (
+    <header className="homepage">
+      <img src={logo} className="App-logo" alt="logo" />
+    </header>
+  );
+};
+
+export default Homepage;
