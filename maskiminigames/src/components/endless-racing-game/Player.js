6ec8e1478d6f35462a638a995821530cf39564ba
Bubseatbubs <bubseatbubs@gmail.com>
2024-12-04 11:37:15 -0800

    Consolidated car logic into CarUtils component

--- maskiminigames/src/components/endless-racing-game/Player.js
+++ maskiminigames/src/components/endless-racing-game/Player.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import playerCarImage from './assets/RedCar.png';
 import { MAP_WIDTH, MAP_HEIGHT } from './Constants'
+import CarUtils from './CarUtils'
 
 const MIN_SPEED = 0.1;
 const MAX_SPEED = 15;
@@ -17,21 +18,12 @@ const IMAGE_HEIGHT = 112;
 class PlayerCar extends Component {
     constructor(props) {
         super(props);
-
-        // Set up variables
-        this.width = IMAGE_WIDTH;
-        this.height = IMAGE_HEIGHT;
+        CarUtils.initialize(this);
 
         this.state = {
             x: MAP_WIDTH / 2 + IMAGE_WIDTH / 4,
             y: -MAP_HEIGHT / 3
         }
-
-        // Get the edges of the track
-        this.trackEdges = {
-            leftEdge : this.width,
-            rightEdge : MAP_WIDTH
-        };
 
         this.verticalSpeed = 1;
         this.horizontalSpeed = 5;
@@ -58,41 +50,17 @@ class PlayerCar extends Component {
     Accelerate or deaccelerate the speed of the player
     */
     accelerate = () => {
-        if (this.verticalSpeed < MAX_SPEED) {
-            this.verticalSpeed += 0.05;
-        }
-
+        CarUtils.accelerate(this, MAX_SPEED);
     }
 
     deaccelerate = () => {
-        if (this.verticalSpeed > MIN_SPEED) {
-            this.verticalSpeed -= 0.05;
-        }
-    }
-
-    // Prints player's position
-    printPosition = () => {
-        console.log(this.state.x + " / " + this.state.y);
+        CarUtils.deaccelerate(this, MIN_SPEED);
     }
 
     // Render out the player
     render() {
         const { x, y } = this.state;
-        return (
-            <div
-                className="player"
-                style={{
-                    position: "relative",
-                    left: `${x}px`,
-                    top: `${y}px`,
-                }}
-            >
-                <img
-                    src={playerCarImage}
-                    alt="PlayerCar"
-                />
-            </div>
-        );
+        return (CarUtils.renderCar(x, y, playerCarImage));
     }
 }
 
--- maskiminigames/src/components/endless-racing-game/Racer.js
+++ maskiminigames/src/components/endless-racing-game/Racer.js
@@ -3,12 +3,9 @@ import blackCarImage from './assets/BlackCar.png';
 import blueCarImage from './assets/BlueCar.png';
 import greenCarImage from './assets/GreenCar.png';
 import pinkCarImage from './assets/PinkCar.png';
-import { MAP_WIDTH, MAP_HEIGHT } from './Constants'
+import { MAP_WIDTH, MAP_HEIGHT, IMAGE_HEIGHT } from './Constants'
+import CarUtils from './CarUtils'
 
-const MIN_SPEED = -30;
-const MAX_SPEED = 30;
-const IMAGE_WIDTH = 64;
-const IMAGE_HEIGHT = 112;
 const RACE_CAR_IMAGES = [
     blackCarImage,
     blueCarImage,
@@ -27,22 +24,12 @@ const RACE_CAR_IMAGES = [
 class Racer extends Component {
     constructor(props) {
         super(props);
-
-        // Set up variables
-        this.width = IMAGE_WIDTH;
-        this.height = IMAGE_HEIGHT;
-
+        CarUtils.initialize(this);
         this.setRandomSpeeds();
-
-        // Get the edges of the track
-        this.trackEdges = {
-            leftEdge : this.width,
-            rightEdge : MAP_WIDTH
-        };
 
         this.state = {
             x: this.randomNumberInRange(this.trackEdges.leftEdge, this.trackEdges.rightEdge),
-            y: MAP_HEIGHT // Bottom of the screen
+            y: -MAP_HEIGHT + IMAGE_HEIGHT // Bottom of the screen
         }
 
         // Set image to a random colored car
@@ -51,24 +38,32 @@ class Racer extends Component {
 
         // Begin constant updates
         this.animationFrameId = requestAnimationFrame(this.update);
+        this.minSpeed = -30;
+        this.maxSpeed = 30;
     };
 
     setRandomSpeeds = () => {
-        this.verticalSpeed = this.randomNumberInRange(1, 3);
-        this.horizontalSpeed = this.randomNumberInRange(-3, 3);
+        this.verticalSpeed = this.randomNumberInRange(-1, -3);
+        this.horizontalSpeed = this.randomNumberInRange(-0.1, 0.1);
     }
 
     // Adds random color and modifies speed somewhat
     // Additionally modifies the horizontal position of the car
     randomizeCarState = () => {
         this.verticalSpeed = this.verticalSpeed + this.randomNumberInRange(-2, 2);
-        this.horizontalSpeed = this.horizontalSpeed + this.randomNumberInRange(-1, 1);
         this.imageIndex = this.randomNumberInRange(0, RACE_CAR_IMAGES.length - 1);
         this.raceCarImage = RACE_CAR_IMAGES[this.imageIndex];
+    }
 
-        this.state = {
-            x: this.randomNumberInRange(this.trackEdges.leftEdge, this.trackEdges.rightEdge)
-        }
+    /*
+    Accelerate or deaccelerate the speed of the player
+    */
+    accelerate = () => {
+        CarUtils.accelerate(this, this.maxSpeed);
+    }
+
+    deaccelerate = () => {
+        CarUtils.deaccelerate(this, this.minSpeed);
     }
 
     // Generates a random number between a given min and max
@@ -76,21 +71,6 @@ class Racer extends Component {
         return Math.floor(Math.random()
             * (max - min + 1)) + min;
     };
-
-    /*
-    Accelerate or deaccelerate the speed of a racer
-    */
-    accelerate = () => {
-        if (this.verticalSpeed < MAX_SPEED) {
-            this.verticalSpeed += 0.05;
-        }
-    }
-
-    deaccelerate = () => {
-        if (this.verticalSpeed > MIN_SPEED) {
-            this.verticalSpeed -= 0.05;
-        }
-    }
 
     update = () => {
         // Apply speed update
@@ -102,6 +82,7 @@ class Racer extends Component {
         // Check if track was crossed
         if (this.state.y < -MAP_HEIGHT) {
             this.setState({
+                x: this.randomNumberInRange(this.trackEdges.leftEdge, this.trackEdges.rightEdge),
                 y: MAP_HEIGHT,
             });
             this.randomizeCarState();
@@ -109,6 +90,7 @@ class Racer extends Component {
         else if (this.state.y > MAP_HEIGHT)
         {
             this.setState({
+                x: this.randomNumberInRange(this.trackEdges.leftEdge, this.trackEdges.rightEdge),
                 y: -MAP_HEIGHT + IMAGE_HEIGHT,
             });
             this.randomizeCarState();
@@ -130,21 +112,7 @@ class Racer extends Component {
     // Render out the racer
     render() {
         const { x, y } = this.state;
-        return (
-            <div
-                className="racer"
-                style={{
-                    position: "relative",
-                    left: `${x}px`,
-                    top: `${y}px`,
-                }}
-            >
-                <img
-                    src={this.raceCarImage}
-                    alt="Racer"
-                />
-            </div>
-        );
+        return (CarUtils.renderCar(x, y, this.raceCarImage));
     }
 }
 
