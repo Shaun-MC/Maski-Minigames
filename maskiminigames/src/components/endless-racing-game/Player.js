c09704bea817bdadc9533bf33fcf0dda6a9b9217
Bubseatbubs <bubseatbubs@gmail.com>
2024-12-03 16:09:47 -0800

    Moved movement logic from the renderer to the player

--- maskiminigames/src/components/endless-racing-game/Player.js
+++ maskiminigames/src/components/endless-racing-game/Player.js
@@ -16,12 +16,22 @@ class PlayerCar extends Component {
             y: 0
         }
 
+        this.isKeyPressed = {
+            ArrowLeft: false,
+            ArrowRight: false,
+            ArrowUp: false,
+            ArrowDown: false,
+        };
+
         this.speed = 5;
         this.width = playerCarImage.width * 0.4;
         this.height = playerCarImage.height * 0.4;
         this.isMoving = false;
     };
 
+    /*
+    Move commands for the player, which move them in a specific direction
+    */
     moveLeft = () => {
         this.setState({
             x: this.state.x - this.speed,
@@ -46,10 +56,12 @@ class PlayerCar extends Component {
         });
     }
 
+    // Prints player's position
     printPosition = () => {
         console.log(this.state.x + " / " + this.state.y);
     }
 
+    // Currently unused
     startMoving = () => {
         this.setState({ isMoving: true });
     };
@@ -57,6 +69,46 @@ class PlayerCar extends Component {
     stopMoving = () => {
         this.setState({ isMoving: false });
     };
+
+    componentDidMount() {
+        window.addEventListener('keydown', this.handleKeyDown);
+        window.addEventListener('keyup', this.handleKeyUp);
+
+        this.animationFrameId = requestAnimationFrame(this.update); // Start the animation loop
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('keydown', this.handleKeyDown);
+        window.removeEventListener('keyup', this.handleKeyUp);
+    }
+
+    handleKeyDown = (event) => {
+        console.log(`Pressed key ${event.key}`)
+        this.isKeyPressed[event.key] = true; // Keep track of keys that are pressed
+        this.printPosition();
+    };
+
+    handleKeyUp = (event) => {
+        console.log(`Stopped pressing key ${event.key}`)
+        this.isKeyPressed[event.key] = false; // Allow movement again once the key is released
+    };
+
+    update = () => {
+        if (this.isKeyPressed['ArrowLeft']) {
+            this.moveLeft(); // Call Player's moveLeft method
+        }
+        if (this.isKeyPressed['ArrowRight']) {
+            this.moveRight(); // Call Player's moveRight method
+        }
+        if (this.isKeyPressed['ArrowUp']) {
+            this.moveUp(); // Call Player's moveUp method
+        }
+        if (this.isKeyPressed['ArrowDown']) {
+            this.moveDown(); // Call Player's moveDown method
+        }
+
+        this.animationFrameId = requestAnimationFrame(this.update); // Start the animation loop
+    }
 
     render() {
         const { x, y } = this.state;
