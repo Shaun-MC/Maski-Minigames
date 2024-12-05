819fa6c2fc016a2a0311af0ad9c8beb78bfe90ff
Bubseatbubs <bubseatbubs@gmail.com>
2024-12-04 23:18:48 -0800

    Change "Play Endless Racing Game" button to "Play Snake" on the Endless Racing Game Page

--- maskiminigames/src/components/endless-racing-game/EndlessRacingGamePage.js
+++ maskiminigames/src/components/endless-racing-game/EndlessRacingGamePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from '../Button.js';
 import homepageButtonImage from '../../assets/homepage_button.png';
-import endlessRacingButtonImage from '../../assets/endless_racing_button.png';
+import snakeButtonImage from '../../assets/snake_button.png';
 import styles from '../../styles/PageStyles.module.css';
 import Renderer from './Renderer'
 
@@ -18,9 +18,9 @@ const EndlessRacingGamePage = () => {
           altText="Go Home"
         />
         <Button
-          link="/endless-racer"
-          imageSrc={endlessRacingButtonImage}
-          altText="Play Endless Racing"
+          link="/snake-game"
+          imageSrc={snakeButtonImage}
+          altText="Play Snake"
         />
       </div>
     </div>
