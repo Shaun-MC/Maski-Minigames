617a18a0a1625b36a240b7b7874a491f1a73bd39
Bubseatbubs <bubseatbubs@gmail.com>
2024-12-05 11:58:39 -0800

    Remove unused useEffect/useState from Renderer

--- maskiminigames/src/components/endless-racing-game/Renderer.js
+++ maskiminigames/src/components/endless-racing-game/Renderer.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect, useState } from 'react';
+import React, { Component } from 'react';
 import PlayerCar from './PlayerCar';
 import RaceTrack from './RaceTrack';
 import Racer from './Racer';
