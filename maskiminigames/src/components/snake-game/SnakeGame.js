59d7f15cc02dc8f98c4e8b05a819f083810f8811
Khushmeet Gobindpuri <khushmeetgobindpuri@gmail.com>
2024-12-04 17:15:59 -0800

    Initial Snake and Apple render

--- maskiminigames/src/components/snake-game/SnakeGame.js
+++ maskiminigames/src/components/snake-game/SnakeGame.js
@@ -10,7 +10,17 @@ import {
 } from "./constants";
 
 const SnakeGame = () => {
-  
+  const canvasRef = useRef();
+  const [snake, setSnake] = useState(SNAKE_START);
+  const [apple, setApple] = useState(APPLE_START);
+
+  // Starting snake moving upward.
+  const [direction, setDirection] = useState([0,-1]);
+
+  const [speed, setSpeed] = useState(null);
+
+  const [gameOver, setGameOver] = useState(false);
+
   const startGame = () => { 
 
   }
@@ -41,8 +51,37 @@ const SnakeGame = () => {
 
   }
 
-  useEffect(() => { 
+  // useEffect draws game objects on the screen
+  useEffect(() => {
+    // Have to get canvas context to draw on canvas
+    const context = canvasRef.current.getContext("2d");
 
+    // setTransform ensures that our game objects
+    // do not scale above a given amount
+    context.setTransform(SCALE, 0, 0, SCALE, 0, 0);
+
+    // context.clearRect(0,0, window.innerWidth, window.innerHeight);
+    context.clearRect(0, 0, CANVAS_SIZE[0], CANVAS_SIZE[1]);
+
+    // Coloring the snake
+    context.fillStyle = "black";
+
+    /*
+    args to fillRect()
+      ==> x,y: current position of each segment of snake
+      ==> 1,1: coloring 1x1 pixel
+    */
+    snake.forEach(([x, y]) => context.fillRect(x, y, 1, 1));
+
+    // Coloring the apple
+    context.fillStyle = "red";
+
+    /*
+    args to fillRect()
+      ==> x,y: current position of apple
+      ==> 1,1: coloring 1x1 pixel
+    */
+    context.fillRect(apple[0], apple[1], 1, 1);
   }, [snake, apple, gameOver])
 
 
@@ -54,8 +93,11 @@ const SnakeGame = () => {
         width={`${CANVAS_SIZE[0]}px`}
         height={`${CANVAS_SIZE[1]}px`}
       /> 
+
+      {/* make separate game over component. */}
+
       {gameOver && <div> {"GAME OVER!!!"} </div>}
-      <button onClick={startGame}> </button>
+      <button onClick={startGame}> START GAME </button>
     </div>
   )
 }
