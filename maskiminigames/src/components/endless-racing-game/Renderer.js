e2c8248f8063c6776c8066efcc78a3e204ad6c48
Ishaan <ikapil004@gmail.com>
2024-12-06 21:13:41 -0800

    updates to renderer.js to use collision manager

--- maskiminigames/src/components/endless-racing-game/Renderer.js
+++ maskiminigames/src/components/endless-racing-game/Renderer.js
@@ -1,135 +1,161 @@
 import React, { Component } from 'react';
 import PlayerCar from './PlayerCar';
 import Racer from './Racer';
+import CollisionManager from './CollisionManager';
+import GameOver from '../snake-game/GameOver';
 import { MAP_WIDTH, MAP_HEIGHT } from './Constants'
 
 // Used to center all of the components that are a part of the game
 const containerStyle = {
-    overflow: 'hidden',
-    backgroundColor: `#717874`,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: `${MAP_HEIGHT}px`,
-    width: `${MAP_WIDTH}px`,
-    margin: '0 auto',
-    position: 'relative',
-    boxSizing: 'border-box'
+   overflow: 'hidden',
+   backgroundColor: `#717874`,
+   display: 'flex',
+   alignItems: 'center',
+   justifyContent: 'center',
+   height: `${MAP_HEIGHT}px`,
+   width: `${MAP_WIDTH}px`,
+   margin: '0 auto',
+   position: 'relative',
+   boxSizing: 'border-box'
 };
 
 /**
- * @class Renderer
- * @description Handles rendering all of the objects in the game.
- * 
- * @author Minh Pham
- * @date December 2024
- */
+* @class Renderer
+* @description Handles rendering all of the objects in the game.
+*
+* @author Minh Pham
+* @date December 2024
+*/
 class Renderer extends Component {
-    constructor(props) {
-        super(props);
+   constructor(props) {
+       super(props);
 
-        // Currently an unused reference but may be used later
-        this.playerRef = React.createRef();
-        this.racerRefs = [];
+       // Currently an unused reference but may be used later
+       this.playerRef = React.createRef();
+       this.racerRefs = [];
 
-        this.isKeyPressed = {
-            ArrowLeft: false,
-            ArrowRight: false,
-            Shift: false,
-            a: false,
-            d: false,
-        };
+       this.isKeyPressed = {
+           ArrowLeft: false,
+           ArrowRight: false,
+           Shift: false,
+           a: false,
+           d: false,
+       };
 
-        this.accelerationInterval = 0;
-    }
+       this.accelerationInterval = 0;
 
-    createRacerRef = (index) => {
-        if (!this.racerRefs[index]) {
-            this.racerRefs[index] = React.createRef();
-        }
-        return this.racerRefs[index];
-    };
 
-    accelerate = () => {
-        this.racerRefs.forEach((racerRef) => racerRef.current.deaccelerate());
-        this.playerRef.current.accelerate();
-    };
+       this.state = {
+           gameOver: false
+       };
+   }
 
-    deaccelerate = () => {
-        this.racerRefs.forEach((racerRef) => racerRef.current.accelerate());
-        this.playerRef.current.deaccelerate();
-    };
+   createRacerRef = (index) => {
+       if (!this.racerRefs[index]) {
+           this.racerRefs[index] = React.createRef();
+       }
+       return this.racerRefs[index];
+   };
 
-    // Setup keypress event listeners to handle movement
-    componentDidMount() {
-        window.addEventListener('keydown', this.handleKeyDown);
-        window.addEventListener('keyup', this.handleKeyUp);
+   accelerate = () => {
+       this.racerRefs.forEach((racerRef) => racerRef.current.deaccelerate());
+       this.playerRef.current.accelerate();
+   };
 
-        this.interval = setInterval(() => {
-            this.accelerate();
-        }, 300);
+   deaccelerate = () => {
+       this.racerRefs.forEach((racerRef) => racerRef.current.accelerate());
+       this.playerRef.current.deaccelerate();
+   };
 
-        this.animationFrameId = requestAnimationFrame(this.update);
-    }
+   // Setup keypress event listeners to handle movement
+   componentDidMount() {
+       window.addEventListener('keydown', this.handleKeyDown);
+       window.addEventListener('keyup', this.handleKeyUp);
 
-    componentWillUnmount() {
-        window.removeEventListener('keydown', this.handleKeyDown);
-        window.removeEventListener('keyup', this.handleKeyUp);
+       this.interval = setInterval(() => {
+           this.accelerate();
+       }, 300);
 
-        clearInterval(this.interval);
-    }
+       this.animationFrameId = requestAnimationFrame(this.update);
+   }
 
-    // Mark key as being pressed down
-    handleKeyDown = (event) => {
-        // handleKeyDown handles holding key presses in an odd manner,
-        // so I instead created a bool to check against, rather than the key
-        // itself.
-        if (this.isKeyPressed[event.key] != null) {
-            event.preventDefault();
-            console.log(`Pressed key ${event.key}`)
-            this.isKeyPressed[event.key] = true;
-        }
-    };
+   componentWillUnmount() {
+       window.removeEventListener('keydown', this.handleKeyDown);
+       window.removeEventListener('keyup', this.handleKeyUp);
 
-    handleKeyUp = (event) => {
-        if (this.isKeyPressed[event.key] != null) {
-            console.log(`Stopped pressing key ${event.key}`)
-            this.isKeyPressed[event.key] = false;
-        }
-    };
+       clearInterval(this.interval);
+   }
 
-    update = () => {
-        if (this.isKeyPressed['ArrowLeft'] || this.isKeyPressed['a']) {
-            this.playerRef.current.moveLeft();
-        }
-        if (this.isKeyPressed['ArrowRight'] || this.isKeyPressed['d']) {
-            this.playerRef.current.moveRight();
-        }
+   // Mark key as being pressed down
+   handleKeyDown = (event) => {
+       // handleKeyDown handles holding key presses in an odd manner,
+       // so I instead created a bool to check against, rather than the key
+       // itself.
+       if (this.isKeyPressed[event.key] != null) {
+           event.preventDefault();
+           console.log(`Pressed key ${event.key}`)
+           this.isKeyPressed[event.key] = true;
+       }
+   };
 
-        if (this.isKeyPressed['Shift']) {
-            this.playerRef.current.slowdownHorizontalSpeed();
-        }
-        else {
-            this.playerRef.current.resetHorizontalSpeed();
-        }
+   handleKeyUp = (event) => {
+       if (this.isKeyPressed[event.key] != null) {
+           console.log(`Stopped pressing key ${event.key}`)
+           this.isKeyPressed[event.key] = false;
+       }
+   };
 
-        this.animationFrameId = requestAnimationFrame(this.update);
-    }
+   update = () => {
+       // Exit if game is already over
+       if (this.state.gameOver) {
+           return;
+       }
+           // Process movement first
+           if (this.isKeyPressed['ArrowLeft'] || this.isKeyPressed['a']) {
+               this.playerRef.current?.moveLeft();
+           }
+           if (this.isKeyPressed['ArrowRight'] || this.isKeyPressed['d']) {
+               this.playerRef.current?.moveRight();
+           }
+           if (this.isKeyPressed['Shift']) {
+               this.playerRef.current?.slowdownHorizontalSpeed();
+           }
+           else {
+               this.playerRef.current?.resetHorizontalSpeed();
+           }
+  
+           // Check for collisions
+           if (CollisionManager.detectCollisions(this.playerRef, this.racerRefs)) {
+               this.setState({ gameOver: true });
+               cancelAnimationFrame(this.animationFrameId);
+               clearInterval(this.interval);
+               return;
+           }
+  
+           // Request next frame only if game isn't over
+           if (!this.state.gameOver) {
+               this.animationFrameId = requestAnimationFrame(this.update);
+           }
+   }
 
-    // Renders out the game
-    render() {
-        return (
-            <div style={containerStyle}>
-                <PlayerCar ref={this.playerRef} />
-                {[...Array(5)].map((_, index) => (
-                    <Racer
-                        key={index}
-                        ref={this.createRacerRef(index)}
-                    />
-                ))}
-            </div>
-        );
-    }
+
+   // Renders out the game
+   render() {
+       if (this.state.gameOver) {
+           return <GameOver />;
+       }
+       return (
+           <div style={containerStyle}>
+               <PlayerCar ref={this.playerRef} />
+               {[...Array(5)].map((_, index) => (
+                   <Racer
+                       key={index}
+                       ref={this.createRacerRef(index)}
+                   />
+               ))}
+           </div>
+       );
+   }
 }
 
 export default Renderer;
