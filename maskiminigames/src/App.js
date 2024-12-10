a2fe4d76de9b58694c5e87d311f6d2230670e37a
Ayleen <ayleenpt@uw.edu>
2024-12-09 16:25:25 -0800

    remove unused import

--- maskiminigames/src/App.js
+++ maskiminigames/src/App.js
@@ -10,7 +10,6 @@ import React from "react";
 import Homepage from "./components/Homepage";
 import EndlessRacingGamePage from "./components/endless-racing-game/EndlessRacingGamePage";
 import SnakeGamePage from "./components/snake-game/SnakeGamePage";
-import SnakeGame from "./components/snake-game/SnakeGame";
 
 function App() {
   return (
