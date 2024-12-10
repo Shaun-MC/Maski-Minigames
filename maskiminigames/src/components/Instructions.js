90f490bcf4bd80a9f31a0a342a569f522cf9b837
Ayleen <ayleenpt@uw.edu>
2024-12-09 18:07:32 -0800

    help button and instructions display components and styles

---
+++ maskiminigames/src/components/Instructions.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from '../styles/PageStyles.module.css';
+import '../styles/Instructions.css';
+
+/**
+ * @component Instructions
+ * @description Formatted instructions
+ * 
+ * @author Ayleen Piteo-Tarpy
+ * @date December 2024
+ */
+
+const Instructions = ({ content, onClose }) => (
+  <div className="instructions-overlay">
+    <div className="instructions-box">
+      <button className="close-button" onClick={onClose}>×</button>
+      
+      <header className="header">INSTRUCTIONS</header>
+      
+      <div className="content">
+        {content}
+      </div>
+    </div>
+  </div>
+);
+
+Instructions.propTypes = {
+  content: PropTypes.element.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
+
+
+export default Instructions;
