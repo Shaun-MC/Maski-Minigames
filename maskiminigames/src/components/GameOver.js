3066fa0e7eedd1572005e78f6ff0cb238b9afbb6
Ayleen <ayleenpt@uw.edu>
2024-12-09 22:08:10 -0800

    add overlay

--- maskiminigames/src/components/GameOver.js
+++ maskiminigames/src/components/GameOver.js
@@ -1,5 +1,6 @@
 import React from "react";
 import StartGameButton from "./StartGameButton";
+import styles from '../styles/Styles.module.css';
 import "../styles/GameOver.css";
 
 /**
@@ -10,17 +11,19 @@ import "../styles/GameOver.css";
  */
 const GameOver = ({ score, startGame }) => {
   return (
-    <div className="game-over">
-      <h1>GAME OVER!!!</h1>
-      <h2>{"FINAL SCORE: " + score}</h2>
-      <div className="buttons">
-        <StartGameButton
-          startGame={startGame}
-          text={"RESTART"}
-        />
-        <button className="menu-button">
-          <a href="/">GO BACK TO MENU</a>
-        </button>
+    <div className={styles.overlay}>
+      <div className="game-over">
+        <h1>GAME OVER!!!</h1>
+        <h2>{"FINAL SCORE: " + score}</h2>
+        <div className="buttons">
+          <StartGameButton
+            startGame={startGame}
+            text={"RESTART"}
+          />
+          <button className="menu-button">
+            <a href="/">GO BACK TO MENU</a>
+          </button>
+        </div>
       </div>
     </div>
   );
