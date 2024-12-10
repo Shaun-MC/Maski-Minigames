c01533e9627c21e3e6a595cbe07029bf5615a196
Ayleen <ayleenpt@uw.edu>
2024-12-09 21:44:53 -0800

    more instructions

--- maskiminigames/src/components/endless-racing-game/InstructionsText.js
+++ maskiminigames/src/components/endless-racing-game/InstructionsText.js
@@ -5,7 +5,8 @@ const InstructionsText = (
     <p>Play as the red car.</p>
     <p>Press 'A' or the left arrow key to move the car left.</p>
     <p>Press 'D' or the right arrow key to move the car right.</p>
-    <p>Try to avoid other cars, or you lose.</p>
+    <p>Hold down 'Shift' to slow down your car.</p>
+    <p>Avoid crashing into other cars.</p>
   </div>
 );
 
