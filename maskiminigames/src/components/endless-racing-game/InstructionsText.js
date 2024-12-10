b95786a5ce4d9d491ea03622e2e32ef16f955c95
Ayleen <ayleenpt@uw.edu>
2024-12-09 17:21:29 -0800

    add instructions files for each game

---
+++ maskiminigames/src/components/endless-racing-game/InstructionsText.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const InstructionsText = (
+  <div>
+    <header>Instructions</header>
+    <p>Play as the red car.</p>
+    <p>Press 'A' or the left arrow key to move the car left.</p>
+    <p>Press 'D' or the right arrow key to move the car right.</p>
+    <p>Try to avoid other cars, or you lose.</p>
+  </div>
+);
+
+export default InstructionsText;
