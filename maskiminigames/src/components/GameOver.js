31a6878a614bc9254a7830db1b77bf7b4f7e4c14
Ayleen <ayleenpt@uw.edu>
2024-12-09 22:02:11 -0800

    move styles into GameOver.css

--- maskiminigames/src/components/GameOver.js
+++ maskiminigames/src/components/GameOver.js
@@ -1,6 +1,6 @@
 import React from "react";
 import StartGameButton from "./StartGameButton";
-
+import "../styles/GameOver.css";
 
 /**
  * @component GameOver
@@ -10,66 +10,19 @@ import StartGameButton from "./StartGameButton";
  */
 const GameOver = ({ score, startGame }) => {
   return (
-    <>
-      <div
-        style={{
-          fontFamily: "monospace",
-          fontSize: "30px",
-          fontWeight: "bold",
-          position: "fixed",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          backgroundColor: "white",
-          padding: "20px",
-          borderRadius: "5px",
-          boxShadow: "0 0 10px rgba(0, 0, 0, 0.5)",
-          zIndex: 1000,
-        }}
-      >
-        <h1
-          style={{
-            color: "red",
-          }}
-        >
-          {"GAME OVER!!!" }
-          <div style={{
-            color:"black"
-          }}> {"FINAL SCORE: " + score}</div>
-        </h1>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-          }}
-        >
-          <StartGameButton
-            startGame={startGame}
-            text={"RESTART"}
-          />
-          <button
-            style={{
-              all: "unset",
-              marginTop: "20px",
-              padding: "6px",
-              borderRadius: "10px",
-              background: "orange",
-            }}
-            onClick={startGame}
-          >
-            <a
-              href="/"
-              style={{
-                textDecoration: "none",
-                color: "inherit",
-              }}
-            >
-              GO BACK TO MENU
-            </a>
-          </button>
-        </div>
+    <div className="game-over">
+      <h1>GAME OVER!!!</h1>
+      <h2>{"FINAL SCORE: " + score}</h2>
+      <div className="buttons">
+        <StartGameButton
+          startGame={startGame}
+          text={"RESTART"}
+        />
+        <button className="menu-button">
+          <a href="/">GO BACK TO MENU</a>
+        </button>
       </div>
-    </>
+    </div>
   );
 };
 
